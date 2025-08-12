ef89dcd8cd29f90669e614aca8a2d946019786bc
Yosua <YosuaMontolaluXD@gmail.com>
2025-08-12 19:55:00 +0800

    Home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,20 @@
 import './globals.css'
+import { Montserrat } from 'next/font/google'
+import { BackgroundBeams } from './../../components/aceternity/background-beams'
 
-export default function RootLayout({
-    children
-}: Readonly<{
-    children: React.ReactNode
-}>) {
+const montserrat = Montserrat({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang='en'>
-            <body>{children}</body>
+            <body className={montserrat.className}>
+                <BackgroundBeams />
+                {children}
+            </body>
         </html>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
+import Link from 'next/link'
+import { ContainerTextFlip } from './../../components/aceternity/container-text-flip'
+
 export default function Home() {
     return (
-        <div>
-            <div>Hello</div>
+        <div className='flex-col gap-y-5 flex h-screen justify-center items-center'>
+            <div className='text-white text-center text-4xl z-10'>
+                <div className='font-light'>
+                    Behind every follow is a story. <br />
+                    Some are real, most are not.
+                </div>
+                <div>
+                    Every profile is a <ContainerTextFlip words={['loyal', 'fake', 'ghost', 'watcher', 'true']} />
+                </div>
+            </div>
+
+            <div className='flex gap-x-5 text-white'>
+                <Link href={'#'} className='p-[3px] relative'>
+                    <div className='absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg' />
+                    <div className='px-8 py-2 bg-black rounded-[6px] relative group transition duration-150 hover:bg-transparent'>
+                        Docs
+                    </div>
+                </Link>
+                <Link
+                    href={'#'}
+                    className='px-6 py-2 flex items-center z-10 bg-black text-white rounded-lg transform hover:-translate-y-1 transition duration-200 border-2 border-transparent hover:border-zinc-600'
+                >
+                    Search by username
+                </Link>
+
+                <Link
+                    href={'#'}
+                    className='px-6 py-2 flex items-center z-10 bg-black text-white rounded-lg transform hover:-translate-y-1 transition duration-200 border-2 border-transparent hover:border-zinc-600'
+                >
+                    Upload
+                </Link>
+            </div>
         </div>
     )
 }
